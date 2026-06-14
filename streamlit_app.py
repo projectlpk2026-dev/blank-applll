51de2f9c6364012032694975ed1912172bb6bb90
projectlpk2026-dev <projectlpk2026@gmail.com>
2026-06-14 16:52:47 +0700

    Add contributor credits to the hero section
    Added a section to credit contributors in the app's hero content.

--- streamlit_app.py
+++ streamlit_app.py
@@ -701,9 +701,25 @@ st.markdown("""
         <div class="logo-core"></div>
     </div>
     <div class="hero-content">
-        <h1>Kalkulator BM / Mr Kimia</h1>
-        <p> Aplikasi kalkulator kimia yang membantu menghitung Bobot Molekul (BM/Mr) senyawa secara cepat, akurat,  mudah, dan dilengkapi  database unsur.</p>
+    <h1>Kalkulator BM / Mr Kimia</h1>
+    <p>Aplikasi kalkulator kimia yang membantu menghitung Bobot Molekul (BM/Mr) senyawa secara cepat, akurat, mudah, dan dilengkapi database unsur.</p>
+
+    <div style="
+        margin-top:20px;
+        padding:15px;
+        background:rgba(255,255,255,0.08);
+        border-left:4px solid #38bdf8;
+        border-radius:12px;
+    ">
+        <h4 style="margin-bottom:10px;">Disusun Oleh:</h4>
+
+        <div>• Alisa Tiara Sani</div>
+        <div>• Alma Zahra Nurika</div>
+        <div>• Najwa Azzahra</div>
+        <div>• Narah Agnia Nugraheni</div>
+        <div>• Wening Ayuning Tyas</div>
     </div>
+  </div>
 </div>
 """, unsafe_allow_html=True)
  
